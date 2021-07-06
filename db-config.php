70bf489e8325f7b6fe3acbb8b9a5b41dd6c71754
ebere <78848941+nancy77zion@users.noreply.github.com>
2021-07-06 14:10:46 +0100

    Add files via upload

---
+++ db-config.php
@@ -0,0 +1,8 @@
+<?php
+
+$databaseHost     = 'localhost';
+$databaseName     = 'register_login_db';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *|Simple PHP Login & Registration Script
+ 
+ */
+?>
+
+<html>
+
+<head>
+
+<body>
+
+    <div>
+        Welcome to Simple Login & Registration Script in PHP.
+    </div>
+    <br /> <br />
+    <div>
+        <a href="login.php"> Login</a> | <a href="register.php"> Register</a>
+    </div>
+
+</body>
+
+</html>
